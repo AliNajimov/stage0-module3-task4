ee4182109c555fbe9362c897ced178adda18a49b
Muhammad Najimov <114020956+AliNajimov@users.noreply.github.com>
2022-10-21 23:09:38 +0500

    Update IntRepresentation.java

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char s1 = 99;
+        char s2 = 104;
+        char s3 = 97;
+        char s4 = 114;
+        System.out.print(s1);
+        System.out.print(s2);
+        System.out.print(s3);
+        System.out.print(s4);
     }
 }
